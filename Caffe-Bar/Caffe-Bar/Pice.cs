433a42c110e83b1bdae060bf50c08a76d5a12f40
dblaskovic99 <dblaskovic99@gmail.com>
2024-01-15 15:03:09 +0100

    napravljene klase

--- Caffe-Bar/Caffe-Bar/Pice.cs
+++ Caffe-Bar/Caffe-Bar/Pice.cs
@@ -8,6 +8,12 @@ namespace Caffe_Bar
 {
     internal class Pice
     {
-
+        public int id_pica {  get; set; }
+        public string naziv_pica { get; set; }
+        public decimal cijena_pica { get; set; }
+        public int id_kategorija_pica { get; set; }
+        public decimal kolicina_kafic { get; set; }
+        public decimal kolicina_skladista {  get; set; }
+        public decimal najmanja_kolicina { get; set; }
     }
 }
