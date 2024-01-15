7ebfc1b6ffc524fadbb15a07806091631d6e6bd2
dblaskovic99 <dblaskovic99@gmail.com>
2024-01-15 18:59:15 +0100

    dodavanje konobara

--- Caffe-Bar/Caffe-Bar/VlasnikForm.Designer.cs
+++ Caffe-Bar/Caffe-Bar/VlasnikForm.Designer.cs
@@ -30,13 +30,29 @@
         {
             this.tabKonobar = new System.Windows.Forms.TabControl();
             this.tabZaposliKonobara = new System.Windows.Forms.TabPage();
+            this.gBoxDodajKonobara = new System.Windows.Forms.GroupBox();
+            this.labelImeKonobara = new System.Windows.Forms.Label();
+            this.btnSpremiKonobara = new System.Windows.Forms.Button();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.labelPrezimeKonobara = new System.Windows.Forms.Label();
+            this.labelLozinkaKonobara = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.labelUsernameKonobar = new System.Windows.Forms.Label();
             this.tabOtpustiKonobara = new System.Windows.Forms.TabPage();
             this.tabUnosPića = new System.Windows.Forms.TabPage();
             this.tabModifikacijaPića = new System.Windows.Forms.TabPage();
             this.tabAkcije = new System.Windows.Forms.TabPage();
             this.tabStatistika = new System.Windows.Forms.TabPage();
             this.gumbOdjavaVlasnika = new System.Windows.Forms.Button();
+            this.btnPrikaziKonobare = new System.Windows.Forms.Button();
+            this.dataGridViewKonobari = new System.Windows.Forms.DataGridView();
             this.tabKonobar.SuspendLayout();
+            this.tabZaposliKonobara.SuspendLayout();
+            this.gBoxDodajKonobara.SuspendLayout();
+            this.tabOtpustiKonobara.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKonobari)).BeginInit();
             this.SuspendLayout();
             // 
             // tabKonobar
@@ -60,6 +76,7 @@
             // tabZaposliKonobara
             // 
             this.tabZaposliKonobara.BackColor = System.Drawing.Color.White;
+            this.tabZaposliKonobara.Controls.Add(this.gBoxDodajKonobara);
             this.tabZaposliKonobara.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabZaposliKonobara.Location = new System.Drawing.Point(4, 54);
             this.tabZaposliKonobara.Margin = new System.Windows.Forms.Padding(5);
@@ -69,8 +86,102 @@
             this.tabZaposliKonobara.TabIndex = 0;
             this.tabZaposliKonobara.Text = "Zaposli konobara";
             // 
+            // gBoxDodajKonobara
+            // 
+            this.gBoxDodajKonobara.Controls.Add(this.labelImeKonobara);
+            this.gBoxDodajKonobara.Controls.Add(this.btnSpremiKonobara);
+            this.gBoxDodajKonobara.Controls.Add(this.txtIme);
+            this.gBoxDodajKonobara.Controls.Add(this.txtLozinka);
+            this.gBoxDodajKonobara.Controls.Add(this.labelPrezimeKonobara);
+            this.gBoxDodajKonobara.Controls.Add(this.labelLozinkaKonobara);
+            this.gBoxDodajKonobara.Controls.Add(this.txtPrezime);
+            this.gBoxDodajKonobara.Controls.Add(this.txtKorisnickoIme);
+            this.gBoxDodajKonobara.Controls.Add(this.labelUsernameKonobar);
+            this.gBoxDodajKonobara.Location = new System.Drawing.Point(75, 42);
+            this.gBoxDodajKonobara.Name = "gBoxDodajKonobara";
+            this.gBoxDodajKonobara.Size = new System.Drawing.Size(714, 375);
+            this.gBoxDodajKonobara.TabIndex = 9;
+            this.gBoxDodajKonobara.TabStop = false;
+            this.gBoxDodajKonobara.Text = "Dodaj konobara";
+            // 
+            // labelImeKonobara
+            // 
+            this.labelImeKonobara.AutoSize = true;
+            this.labelImeKonobara.Location = new System.Drawing.Point(167, 53);
+            this.labelImeKonobara.Name = "labelImeKonobara";
+            this.labelImeKonobara.Size = new System.Drawing.Size(53, 29);
+            this.labelImeKonobara.TabIndex = 0;
+            this.labelImeKonobara.Text = "Ime";
+            // 
+            // btnSpremiKonobara
+            // 
+            this.btnSpremiKonobara.Location = new System.Drawing.Point(370, 309);
+            this.btnSpremiKonobara.Name = "btnSpremiKonobara";
+            this.btnSpremiKonobara.Size = new System.Drawing.Size(140, 37);
+            this.btnSpremiKonobara.TabIndex = 8;
+            this.btnSpremiKonobara.Text = "Zaposli!";
+            this.btnSpremiKonobara.UseVisualStyleBackColor = true;
+            this.btnSpremiKonobara.Click += new System.EventHandler(this.btnSpremiKonobara_Click);
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(226, 48);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(284, 34);
+            this.txtIme.TabIndex = 1;
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(226, 255);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(284, 34);
+            this.txtLozinka.TabIndex = 7;
+            // 
+            // labelPrezimeKonobara
+            // 
+            this.labelPrezimeKonobara.AutoSize = true;
+            this.labelPrezimeKonobara.Location = new System.Drawing.Point(118, 115);
+            this.labelPrezimeKonobara.Name = "labelPrezimeKonobara";
+            this.labelPrezimeKonobara.Size = new System.Drawing.Size(102, 29);
+            this.labelPrezimeKonobara.TabIndex = 2;
+            this.labelPrezimeKonobara.Text = "Prezime";
+            // 
+            // labelLozinkaKonobara
+            // 
+            this.labelLozinkaKonobara.AutoSize = true;
+            this.labelLozinkaKonobara.Location = new System.Drawing.Point(125, 258);
+            this.labelLozinkaKonobara.Name = "labelLozinkaKonobara";
+            this.labelLozinkaKonobara.Size = new System.Drawing.Size(95, 29);
+            this.labelLozinkaKonobara.TabIndex = 6;
+            this.labelLozinkaKonobara.Text = "Lozinka";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(226, 112);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(284, 34);
+            this.txtPrezime.TabIndex = 3;
+            // 
+            // txtKorisnickoIme
+            // 
+            this.txtKorisnickoIme.Location = new System.Drawing.Point(226, 183);
+            this.txtKorisnickoIme.Name = "txtKorisnickoIme";
+            this.txtKorisnickoIme.Size = new System.Drawing.Size(284, 34);
+            this.txtKorisnickoIme.TabIndex = 5;
+            // 
+            // labelUsernameKonobar
+            // 
+            this.labelUsernameKonobar.AutoSize = true;
+            this.labelUsernameKonobar.Location = new System.Drawing.Point(48, 183);
+            this.labelUsernameKonobar.Name = "labelUsernameKonobar";
+            this.labelUsernameKonobar.Size = new System.Drawing.Size(172, 29);
+            this.labelUsernameKonobar.TabIndex = 4;
+            this.labelUsernameKonobar.Text = "Korisničko ime";
+            // 
             // tabOtpustiKonobara
             // 
+            this.tabOtpustiKonobara.Controls.Add(this.dataGridViewKonobari);
+            this.tabOtpustiKonobara.Controls.Add(this.btnPrikaziKonobare);
             this.tabOtpustiKonobara.Location = new System.Drawing.Point(4, 54);
             this.tabOtpustiKonobara.Margin = new System.Windows.Forms.Padding(5);
             this.tabOtpustiKonobara.Name = "tabOtpustiKonobara";
@@ -130,6 +241,26 @@
             this.gumbOdjavaVlasnika.Text = "Odjava";
             this.gumbOdjavaVlasnika.UseVisualStyleBackColor = true;
             // 
+            // btnPrikaziKonobare
+            // 
+            this.btnPrikaziKonobare.Location = new System.Drawing.Point(159, 69);
+            this.btnPrikaziKonobare.Name = "btnPrikaziKonobare";
+            this.btnPrikaziKonobare.Size = new System.Drawing.Size(266, 40);
+            this.btnPrikaziKonobare.TabIndex = 0;
+            this.btnPrikaziKonobare.Text = "Prikaži konobare";
+            this.btnPrikaziKonobare.UseVisualStyleBackColor = true;
+            this.btnPrikaziKonobare.Click += new System.EventHandler(this.btnPrikaziKonobare_Click);
+            // 
+            // dataGridViewKonobari
+            // 
+            this.dataGridViewKonobari.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKonobari.Location = new System.Drawing.Point(37, 115);
+            this.dataGridViewKonobari.Name = "dataGridViewKonobari";
+            this.dataGridViewKonobari.RowHeadersWidth = 51;
+            this.dataGridViewKonobari.RowTemplate.Height = 24;
+            this.dataGridViewKonobari.Size = new System.Drawing.Size(522, 375);
+            this.dataGridViewKonobari.TabIndex = 1;
+            // 
             // VlasnikForm
             // 
             this.ClientSize = new System.Drawing.Size(1238, 630);
@@ -137,6 +268,11 @@
             this.Controls.Add(this.tabKonobar);
             this.Name = "VlasnikForm";
             this.tabKonobar.ResumeLayout(false);
+            this.tabZaposliKonobara.ResumeLayout(false);
+            this.gBoxDodajKonobara.ResumeLayout(false);
+            this.gBoxDodajKonobara.PerformLayout();
+            this.tabOtpustiKonobara.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKonobari)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -151,5 +287,17 @@
         private System.Windows.Forms.TabPage tabAkcije;
         private System.Windows.Forms.TabPage tabStatistika;
         private System.Windows.Forms.Button gumbOdjavaVlasnika;
+        private System.Windows.Forms.Label labelImeKonobara;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Label labelLozinkaKonobara;
+        private System.Windows.Forms.TextBox txtKorisnickoIme;
+        private System.Windows.Forms.Label labelUsernameKonobar;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Label labelPrezimeKonobara;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Button btnSpremiKonobara;
+        private System.Windows.Forms.GroupBox gBoxDodajKonobara;
+        private System.Windows.Forms.DataGridView dataGridViewKonobari;
+        private System.Windows.Forms.Button btnPrikaziKonobare;
     }
 }
--- Caffe-Bar/Caffe-Bar/VlasnikForm.cs
+++ Caffe-Bar/Caffe-Bar/VlasnikForm.cs
@@ -2,19 +2,85 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace Caffe_Bar
 {
     public partial class VlasnikForm : Form
     {
+        static int id_konobara = 6;
+        static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\\\wsl.localhost\\Ubuntu-18.04\\home\\doriblas\\Caffe-Bar\\Caffe-Bar\\Caffe-Bar\\baza.mdf;Integrated Security=True";
         public VlasnikForm()
         {
             InitializeComponent();
+        }
+
+        private void btnSpremiKonobara_Click(object sender, EventArgs e)
+        {
+            foreach (Control kontrola in gBoxDodajKonobara.Controls)
+            {
+                if (kontrola is TextBox t && t.Text.Length == 0)
+                {
+                    MessageBox.Show("Nisu popunjena sva polja!");
+                    return;
+                }
+            }
+            Osoba konobar = new Osoba()
+            { 
+                id_osobe = 10,
+                ime = txtIme.Text,
+                prezime = txtPrezime.Text,
+                uloga = 1,
+                korisnicko_ime = txtKorisnickoIme.Text,
+                lozinka = txtLozinka.Text
+            };
+            SqlConnection veza = new SqlConnection(connectionString);
+            veza.Open();
+
+            string upit = "INSERT INTO "
+                + "Osobe (id_osobe, ime, prezime, uloga, korisnicko_ime, lozinka) "
+                + "VALUES (@id_osobe, @ime, @prezime, @uloga, @korisnicko_ime, @lozinka)";
+            SqlCommand naredba = new SqlCommand(upit, veza);
+            naredba.Parameters.AddWithValue("@id_osobe", konobar.id_osobe);
+            naredba.Parameters.AddWithValue("@ime", konobar.ime);
+            naredba.Parameters.AddWithValue("@prezime", konobar.prezime);
+            naredba.Parameters.AddWithValue("@uloga", konobar.uloga);
+            naredba.Parameters.AddWithValue("@korisnicko_ime", konobar.korisnicko_ime);
+            naredba.Parameters.AddWithValue("@lozinka", konobar.lozinka);
+
+            try
+            {
+                naredba.ExecuteNonQuery();
+                MessageBox.Show("Konobar uspješno zaposlen!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Došlo je do greške: " + ex.Message);
+            }
+
+            veza.Close();
+        }
+
+        private void btnPrikaziKonobare_Click(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(connectionString);
+
+            veza.Open();
+
+            string upit = "SELECT ime, prezime, korisnicko_ime FROM Osobe WHERE uloga = 1"; 
+            SqlDataAdapter adapter = new SqlDataAdapter(upit, veza);
+            DataTable dt  = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridViewKonobari.DataSource = dt;
+
+            veza.Close();
         }
     }
 }
