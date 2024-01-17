f3ccddbf1307170aad4650123bc02b6acb862608
ikrizanec <ivanakrizanec@hotmail.com>
2024-01-17 02:35:23 +0100

    Dodani potrebni update na bazu nakon izdavanja računa.

--- Caffe-Bar/Caffe-Bar/IzdajRacun.cs
+++ Caffe-Bar/Caffe-Bar/IzdajRacun.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CaffeBar
 {
     public partial class IzdajRacun : Form
     {
+        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ivana\Desktop\RP\Caffe-Bar\Caffe-Bar\Caffe-Bar\baza.mdf;Integrated Security=True";
         private decimal Total { get; set; }
-        private DateTime VrijemeRacuna { get; set; }
+        public DateTime VrijemeRacuna { get; private set; }
+        
+        public int Id_konobar { get; private set; }
 
         public IzdajRacun()
         {
@@ -21,14 +26,21 @@ namespace CaffeBar
             finalniRacun.SelectionAlignment = HorizontalAlignment.Right;
             finalniRacun.AppendText("\n\n");
             finalniRacun.AppendText("-------------------------------------\n\n");
-            finalniRacun.AppendText("Ukupna cijena: " + Total.ToString());
-            finalniRacun.AppendText("\n\n");
-            finalniRacun.AppendText(VrijemeRacuna + "\n");
-            finalniRacun.AppendText("Wifi password: caffebar123\n\n");
+            finalniRacun.AppendText("Ukupna cijena: " + Total.ToString() + "\n\n");
             finalniRacun.AppendText("--------------------------------------\n\n");
-            finalniRacun.SelectionAlignment = HorizontalAlignment.Left;
+            finalniRacun.AppendText(VrijemeRacuna + "\n\n");
+            finalniRacun.AppendText("Wifi password: caffebar123\n\n");
         }
-        
+
+        public void updateKonobarInfo(int id_konobara, string ime, string prezime)
+        {
+            Id_konobar = id_konobara;
+            string racunPotpis = "Konobar: " + ime + " " + prezime + "\n\n";
+            finalniRacun.AppendText(racunPotpis);
+            finalniRacun.AppendText("--------------------------------------\n\n");
+        }
+
+
         public decimal calculateTotal(Dictionary<Pice, decimal> narucenaPica)
         {
             decimal total = 0;
@@ -71,10 +83,36 @@ namespace CaffeBar
 
         private void gumbIzdajRacun_Click(object sender, EventArgs e)
         {
-            // zapisi racun
+            SqlConnection veza = new SqlConnection(connectionString);
+            veza.Open();
 
-            //ako sve proslo okej
-            DialogResult = DialogResult.OK;
+            string upit = "INSERT INTO "
+                + "Racun (id_konobar, iznos, datum_vrijeme) "
+                + "VALUES (@id_konobar, @iznos, @datum_vrijeme)";
+            SqlCommand naredba = new SqlCommand(upit, veza);
+            naredba.Parameters.AddWithValue("@id_konobar", Id_konobar);
+            naredba.Parameters.AddWithValue("@iznos", Total);
+            naredba.Parameters.AddWithValue("@datum_vrijeme", VrijemeRacuna);
+
+            try
+            {
+                int brojDodanih = naredba.ExecuteNonQuery();
+                if(brojDodanih > 0)
+                {
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("Nije dodan nijedan red.","Greška");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Došlo je do greške: " + ex.Message);
+            }
+            veza.Close();
         }
+            
     }
 }
