1598f83a9a9f56f6abc46f076bc08f193d8033ab
helenajeresic <helenajeresic12@gmail.com>
2024-01-18 17:43:26 +0100

    dizajn

--- Caffe-Bar/Caffe-Bar/ObracunForm.cs
+++ Caffe-Bar/Caffe-Bar/ObracunForm.cs
@@ -14,7 +14,7 @@ namespace CaffeBar
 {
     public partial class ObracunForm : Form
     {
-        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Elena\Desktop\Caffe-Bar-novo\Caffe-Bar\Caffe-Bar\baza.mdf;Integrated Security=True;MultipleActiveResultSets=True;";
+        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Helena\Desktop\moje\Caffe-Bar\Caffe-Bar\baza.mdf;Integrated Security=True;MultipleActiveResultSets=True;";
         public int id_ulogirani;
         public ObracunForm(int id_konobar)
         {
